0020d674bf5378da81b93bc65343a7a17ca24ef6
leehyeongjin <e93052080@gmail.com>
2021-02-19 15:14:34 +0900

    javascript:scroll to section

--- portfolioweb/main.js
+++ portfolioweb/main.js
@@ -1,1 +1,33 @@
-//font-family: 'Open Sans', sans-serif;
+'use strict'
+
+//Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu')
+navbarMenu.addEventListener('click',()=>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth', block: "center"});
+});
+
+//
+// const navbarMenu = document.querySelector('.navbar__menu')
+// navbarMenu.addEventListener('click',()=>{
+//     const target = event.target;
+//     const link = target.dataset.link;
+//     if (link == null){
+//         return;
+//     }
